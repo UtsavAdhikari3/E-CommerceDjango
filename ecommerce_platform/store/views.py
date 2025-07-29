909a00c14875e561a2bf29c7b0ab76f55c881644
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2025-07-29 19:47:17 +0545

    serializer and view

--- ecommerce_platform/store/views.py
+++ ecommerce_platform/store/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView,RetrieveAPIView
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ListCreateProducts(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class RetrieveProduct(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
+    
