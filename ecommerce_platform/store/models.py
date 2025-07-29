15334199f74e0ae5ca66b249a662e9dab25a0374
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2025-07-29 19:21:14 +0545

    product model

--- ecommerce_platform/store/models.py
+++ ecommerce_platform/store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator,MinValueValidator
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField(max_length=200)
+    price = models.DecimalField(max_digits=8,decimal_places=2)
+    in_stock = models.BooleanField()
+    quantity = models.IntegerField(validators=[MaxValueValidator(1000),MinValueValidator(1)])
+    created_date = models.DateField(auto_now_add=True)
+    image = models.ImageField()
