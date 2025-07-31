a782ce0313b0c9b539a1fc144fa41dbf877d9562
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2025-07-31 08:36:53 +0545

    serializer fix

--- ecommerce_platform/store/urls.py
+++ ecommerce_platform/store/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('product/',views.ListCreateProducts.as_view(),name='ListProducts'),
-    path('product/<int:pk>',views.RetrieveProduct.as_view(),name='ListProduct')
+    path('products/',views.ListCreateProducts.as_view(),name='ListProducts'),
+    path('products/<int:id>',views.RetrieveProduct.as_view(),name='ListProduct')
 ]
