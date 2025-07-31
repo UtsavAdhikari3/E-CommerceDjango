b8fdf06995e17f74cf10d17120a36e5af1b56006
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2025-07-31 19:41:26 +0545

    cart serializer

--- ecommerce_platform/store/serializers.py
+++ ecommerce_platform/store/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, CartItem, Cart
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(read_only=True)
+    subtotal = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CartItem
+        fields = ['products','quantity','subtotal']
+
+    def get_subtotal(self,obj):
+        return obj.quantity * obj.products.price
+
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+    total = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cart
+        fields = ['items', 'total']
+
+    def get_total(self, obj):
+        return sum(item.products.price * item.quantity for item in obj.items.all())
+
